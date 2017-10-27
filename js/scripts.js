f51d0422e73d931372b22b1eb1e10eb1f55ceecd
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-10-27 15:47:03 -0700

    make me feel pretty frustrated at this point. not able to get first string to loop again...

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Business Logic
 var counter = function(inputNumber) {
+  debugger;
   var numList = [];
   for (var index = 0; index <= inputNumber; index++)
     numList.push(index);
@@ -7,25 +8,20 @@ var counter = function(inputNumber) {
     return string;
 }
 
-
 var translator = function(string) {
   var newString = [];
   for (var i = 0; i <= string.length; i++) {
-    if (i % 3 === 0) {
-      string[i] = "Sorry";
-    } else if (i === 1) {
+    if (i === "1") {
       string[i] = "boop";
-    } else if (i === 0) {
+    } else if (i === "0") {
       string[i] = "beep";
+    } else (i % 3 === "0") {
+      string[i] = "Sorry";
     }
-    newString.push(i);
-    return newString;
+      newString.push(i);
+      return newString;
   }
 }
-
-
-
-
 
 
 // User Interface Logic
@@ -33,16 +29,14 @@ $(document).ready(function() {
   $(".jumbotron a").click(function() {
     $(".survey").show();
   });
+
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var inputNum = parseInt($("input#inputNumber").val());
-    console.log(inputNum);
-    var firstResult = counter(inputNum);
-    console.log(firstResult);
-    var finalResult = translator(firstResult);
-    console.log(finalResult);
+
+    var result = translator(counter(inputNum));
 
 
-    $("#listarea").empty().append(finalResult)
+    $("#listarea").empty().text(finalResult);
   });
 });
