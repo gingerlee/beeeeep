6747e218f13e2a38dbf59e5cefc5ced8c91011a7
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-10-27 12:00:32 -0700

    struggling to find the method to make array of integers into a string, and then into separate digits

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,30 @@ var counter = function(input) {
   for (var index = 0; index <= input; index++)
     numList.push(index);
     return numList;
-
 }
+// if (index % 3 === 0) {
+//   numList[index] = "Sorry";
+// } else if (index === 1) {
+//   numList[index] = "boop";
+// } else (index === 0) {
+//   numList[index] = "beep";
+// }
+
+
 
 
 
 // User Interface Logic
 $(document).ready(function() {
+  $(".jumbotron a").click(function() {
+    $(".survey").show();
+  });
   $("form#translator").submit(function(event) {
     event.preventDefault();
+    var inputNum = parseInt($("input#inputNumber").val());
+    var result = counter(inputNum)
+
+
+    $("#listarea").empty().append(result)
   });
 });
