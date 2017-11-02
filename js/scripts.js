4ed5175a92259a992128688ea42ea3c3b2d90e9c
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-02 07:06:08 -0700

    update branching logic so 12 rule is first

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ var beeper = function(input) {
     return alert("Please enter a number.");
   }
   var output = [];
-  for (var index = 1; index <= input; index++) {
-    if ((index % 3) === 0) {
+  for (var index = 1; index <= input; index++) { //unsure how to start at 0 and return 0 first
+    if ((index % 12) === 0) {
+      output.push("latte");
+    } else if ((index % 3) === 0) {
       output.push("pumpkin");
     } else if ((index % 4) === 0) {
       output.push("spice");
-    } else if ((index % 12) === 0) {
-      output.push("latte");
     } else {
       output.push(index);
     }
