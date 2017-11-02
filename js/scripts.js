526e961adf3fc066056488c2e53491b926d02a93
Guest User <Guest@Epicodus-9E.local>
2017-11-02 11:09:18 -0700

    add ability to show at start

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ var beeper = function(input) {
     return alert("Please enter a number.");
   }
   var output = [];
-  for (var index = 1; index <= input; index++) { //unsure how to start at 0 and return 0 first
-    if ((index % 12) === 0) {
+  for (var index = 0; index <= input; index++) { 
+    if ((index % 12) === 0 && index > 0) {
       output.push("latte");
-    } else if ((index % 3) === 0) {
+    } else if ((index % 3) === 0 && index > 0) {
       output.push("pumpkin");
-    } else if ((index % 4) === 0) {
+    } else if ((index % 4) === 0 && index > 0) {
       output.push("spice");
     } else {
       output.push(index);
