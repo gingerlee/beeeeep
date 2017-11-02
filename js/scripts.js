d6829440801174e0b9c9939a92525580f017eed7
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-02 06:35:29 -0700

    set up initial branching logic, currently not working

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,19 @@ var beeper = function(input) {
   if (isNaN(input) === true || input < 1) {
     return alert("Please enter a number.");
   }
+  var output = [];
+  for (index = 0; index >= input; index++)
+  if ((input % 12) === 0) {
+    output.push("latte");
+  } else if ((input % 3) === 0) {
+    output.push("pumpkin");
+  }  else if ((input % 4) === 0) {
+    output.push("spice");
+  }
+  return output;
+  console.log(output);
+};
 
-  
 // User Interface Logic
 $(document).ready(function() {
   $("button").click(function() {
@@ -15,7 +26,8 @@ $(document).ready(function() {
   $("form#beep").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#input").val());
+    var output = beeper(input);
 
-    $("#listarea").empty().text(beeper(input));
+    $("#listarea").empty().text(output);
   });
 });
